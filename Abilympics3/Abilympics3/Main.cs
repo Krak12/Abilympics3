872e16065cfd7323f1028a7db4b4934d1ffec358
Krak12 <mamukin08@gmail.com>
2019-10-10 11:49:59 +0300

    Change code and forms

--- Abilympics3/Abilympics3/Main.cs
+++ Abilympics3/Abilympics3/Main.cs
@@ -16,5 +16,65 @@ namespace Abilympics3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // настройка ввода логина и пароля
+            var userTA = new dbDataSetTableAdapters.WorkersTableAdapter();
+            var users = userTA.GetDataByLoginAndPass(textBox1.Text, textBox2.Text);
+
+            if (users.Count == 0)
+            {
+                MessageBox.Show("", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // переходы по формам, зависящие от TypeAcc
+            Data.UserAutorized = userTA.GetDataByLogin(textBox1.Text.Trim()).First();
+
+            if (Data.UserAutorized.TypeAcc == 1)
+            {
+                Form cm = new CreatorMenu();
+                Hide();
+                DialogResult res = cm.ShowDialog();
+                if (res != DialogResult.Cancel)
+                {
+                    Show();
+                }
+                else Close();
+            }
+            else if (Data.UserAutorized.TypeAcc == 2)
+            {
+                Form sm = new SpecialistMenu();
+                Hide();
+                DialogResult res = sm.ShowDialog();
+                if (res != DialogResult.Cancel)
+                {
+                    Show();
+                }
+                else Close();
+            }
+            else if (Data.UserAutorized.TypeAcc == 3)
+            {
+                Form tm = new TechnicianMenu();
+                Hide();
+                DialogResult res = tm.ShowDialog();
+                if (res != DialogResult.Cancel)
+                {
+                    Show();
+                }
+                else Close();
+            }
+        }
+
+        // настройка функции Показать пароль
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = !checkBox1.Checked;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = true;
+        }
     }
 }
